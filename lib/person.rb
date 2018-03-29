cfe642726e26be1e1c8be9c0562a34708943c6ac
Michael Roth <mick2090@gmail.com>
2018-03-29 21:56:39 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,17 @@
 class Person
-  def initialize=(name)
-    @name = name
-  end
-  
-  def name=(name)
-    @name = name
+  def name=(person_name)
+    @name = person_name
   end
   
   def name
     @name
   end
   
+  def job=(person_job)
+    @job = person_job
+  end
+  
+  def job
+    @job
+  end
 end
